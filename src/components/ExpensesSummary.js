404da9036095a70066d329d3af94ba5ff8f0ea89
Vikram <vickramskadam@gmail.com>
2020-09-11 11:50:15 +0530

    Add expense summary to dashboard page

---
+++ src/components/ExpensesSummary.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import numeral from 'numeral';
+import selectExpenses from '../selectors/expenses';
+import selectExpensesTotal from '../selectors/expenses-total';
+
+const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+    const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+    const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+    return(
+        <div>
+            <h1>Viewing {expenseCount} {expenseWord} totalling of {formattedExpensesTotal} </h1>
+        </div>
+    )
+};
+
+const mapStateToProps = (state) => {
+    const visibleExpense = selectExpenses(state.expenses, state.filters);
+    return{
+        expenseCount: visibleExpense.length,
+        expensesTotal: selectExpensesTotal(visibleExpense)
+    }
+};
+
+export default connect(mapStateToProps)(ExpensesSummary);
